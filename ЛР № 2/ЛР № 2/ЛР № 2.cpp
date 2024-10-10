62b478674a0191e3055285b18a96a8dc741afdc5
Benik800 <127986684+Benik800@users.noreply.github.com>
2024-10-10 07:14:05 +0500

    Add files via upload

--- ЛР № 2/ЛР № 2/ЛР № 2.cpp
+++ ЛР № 2/ЛР № 2/ЛР № 2.cpp
@@ -4,7 +4,6 @@
 #include <ctime>
 #include <cmath>
 #include <sstream>
-#include <regex>
 #include <map>
 #include <cstdlib>
 #include "encrypt.h"
@@ -12,24 +11,24 @@
 
 using namespace std;
 
-string key = "i1want2to3pass4cgi";
+string keyWord = "i1want2to3pass4cgi";
 
 string create_encryption_data(const string& data) 
 {
     string s = data;
-    return encrypt_vigenere(s, key);
+    return encrypt_vigenere(s, keyWord);
 }
 
 string create_encryption_Number(int num) 
 {
     string s = to_string(num);
-    return encrypt_vigenere(s, key);
+    return encrypt_vigenere(s, keyWord);
 }
 
 string decryption_data(string s) 
 {
-    string newKey = extend_key(s, key);
-    string number = decrypt_vigenere(s, newKey);
+    string newKeyWord = extend_key(s, keyWord);
+    string number = decrypt_vigenere(s, newKeyWord);
     return number;
 }
 
@@ -44,7 +43,7 @@ string decURL(const string& str)
             if (i + 2 < str.length()) 
             {
                 string value = str.substr(i + 1, 2);
-                decodeURL += char(stoi(value, nullptr, 16));
+                decodeURL += char(stoi(value, NULL, 16));
                 i += 2;
             }
         }
@@ -79,6 +78,27 @@ map<string, string> parsing(const string& request)
     return elements;
 }
 
+string textAttempts(int Attempts) 
+{
+    if (Attempts == 1) 
+    {
+        return "У вас осталась 1 попытка.";
+    }
+    else if(Attempts == 0)
+    {
+        return "У вас не осталось попыток! ";
+    }
+    else if (Attempts >= 2 && Attempts <= 4) 
+    {
+        return "У вас осталось " + to_string(Attempts) + " попытки.";
+    }
+    else 
+    {
+        return "У вас осталось " + to_string(Attempts) + " попыток.";
+    }
+
+}
+
 int main() {
 
     srand(time(NULL));
@@ -88,33 +108,38 @@ int main() {
 
     const int min = 1;
     const int max = 100;
+
     int guessedNumber = rand() % max + min;
-    string hashedNumber = create_encryption_Number(guessedNumber);
+    string encryptionNumber = create_encryption_Number(guessedNumber);
+
     const int maxAttempts = ceil(log2(max - min + 1));
     int currentAttempts = maxAttempts;
-    string hashedAttempts = (create_encryption_Number(currentAttempts));
+    string encryptionAttempts = (create_encryption_Number(currentAttempts));
     string prevAttempts = "";
+
+    string control_sum;
     string message = "";
+
     bool cheat = false;
     bool victory = false;
-    string control_sum;
 
-    if (request_method && string(request_method) == "POST") 
+
+    if (request_method && string(request_method) == "POST")
     {
-        if (content_length) 
+        if (content_length)
         {
             int length = stoi(content_length);
             string text(length, ' ');
             cin.read(&text[0], length);
             map<string, string> elements = parsing(text);
-            if (elements.count("hashedAttempts") && elements.count("hashedNumber") && elements.count("ControlSum") && elements.count("prevAttempts"))
+            if (elements.count("encryptionAttempts") && elements.count("encryptionNumber") && elements.count("ControlSum") && elements.count("prevAttempts"))
             {
                 control_sum = elements["ControlSum"];
-                string NewHashedNumber = elements["hashedNumber"];
+                string NewHashedNumber = elements["encryptionNumber"];
                 string NewPrevAttempts = elements["prevAttempts"];
-                string NewHashedAttempts = elements["hashedAttempts"];
+                string NewHashedAttempts = elements["encryptionAttempts"];
                 string NewControlSum = NewHashedAttempts + NewHashedNumber + NewPrevAttempts;
-                if (create_encryption_data(NewControlSum) != control_sum) 
+                if (create_encryption_data(NewControlSum) != control_sum)
                 {
                     message = "Жульничать плохо! Игра окончена!";
                     currentAttempts = 0;
@@ -126,53 +151,54 @@ int main() {
                     message = "Игра окончена! Начните заново!";
                     prevAttempts = " ";
                 }
-                else 
+                else
                 {
-                    hashedNumber = NewHashedNumber;
+                    encryptionNumber = NewHashedNumber;
                     prevAttempts = NewPrevAttempts;
                     currentAttempts = stoi(decryption_data(NewHashedAttempts));
                 }
             }
 
-            if (elements.count("usernum") && cheat == false && currentAttempts > 0) 
+            if (elements.count("usernum") && cheat == false && currentAttempts > 0)
             {
                 string userNumberStr = elements["usernum"];
                 int userNumber = stoi(userNumberStr);
-                guessedNumber = stoi(decryption_data(hashedNumber));
+                guessedNumber = stoi(decryption_data(encryptionNumber));
                 string hashedUserNumber = create_encryption_Number(userNumber);
 
-                if (userNumber < min || userNumber > max) 
+                if (userNumber < min || userNumber > max)
                 {
                     message = "Выход за пределы!";
+                    encryptionAttempts = create_encryption_Number(currentAttempts);
                 }
 
-                else 
+                else
                 {
                     if (currentAttempts > 0)
                     {
                         --currentAttempts;
-                        hashedAttempts = create_encryption_Number(currentAttempts);
+                        encryptionAttempts = create_encryption_Number(currentAttempts);
                     }
 
-                    if (hashedUserNumber == hashedNumber && currentAttempts >= 0)
+                    if (hashedUserNumber == encryptionNumber && currentAttempts >= 0)
                     {
                         victory = true;
-                        prevAttempts +=  to_string(userNumber) + " ";
+                        prevAttempts += to_string(userNumber) + " ";
                         message = "Вы угадали! Было загадано число " + to_string(guessedNumber);
 
                     }
 
-                    else if(userNumber < guessedNumber && currentAttempts > 0)
+                    else if (userNumber < guessedNumber && currentAttempts > 0)
                     {
                         prevAttempts += to_string(userNumber) + ", ";
                         message = "Загаданное число больше! ";
                     }
-                    else if (userNumber > guessedNumber && currentAttempts > 0) 
+                    else if (userNumber > guessedNumber && currentAttempts > 0)
                     {
                         prevAttempts += to_string(userNumber) + ", ";
                         message = "Загаданное число меньше! ";
                     }
-                    else if (currentAttempts == 0) 
+                    else if (currentAttempts == 0)
                     {
                         message = "Игра окончена! Начните заново! Было загадано число " + to_string(guessedNumber);
                         prevAttempts += to_string(userNumber) + " ";
@@ -185,7 +211,7 @@ int main() {
         }
     }
 
-    control_sum = create_encryption_data(hashedAttempts + hashedNumber + prevAttempts);
+    control_sum = create_encryption_data(encryptionAttempts + encryptionNumber + prevAttempts);
 
     cout << "Content-type: text/html " << endl << endl;
     cout << "<!DOCTYPE html>" << endl;
@@ -212,8 +238,8 @@ int main() {
     cout << "</p>" << endl;
     cout << "<form id=\"guessForm\" class=\"d-flex justify-content-between align-items-start\">" << endl;
     cout << "<div class=\"d-flex form-group\">" << endl;
-    cout << "<input type=\"hidden\" name=\"hashedNumber\" value=\"" << hashedNumber << "\">";
-    cout << "<input type=\"hidden\" name=\"hashedAttempts\" value=\"" << hashedAttempts << "\">";
+    cout << "<input type=\"hidden\" name=\"encryptionNumber\" value=\"" << encryptionNumber << "\">";
+    cout << "<input type=\"hidden\" name=\"encryptionAttempts\" value=\"" << encryptionAttempts << "\">";
     cout << "<input type=\"hidden\" name=\"prevAttempts\" value=\"" << prevAttempts << "\">";
     cout << "<input type=\"hidden\" name=\"ControlSum\" value=\"" << control_sum << "\">";
     cout << "<input type=\"number\" name=\"usernum\" class=\"form-control\" id=\"guess\" placeholder=\"Введите число\">" << endl;
@@ -230,7 +256,7 @@ int main() {
     cout << "<div class=\"container other d-flex flex-column align-items-start mt-2\">" << endl;
     cout << "<p class=\"answer\" id=\"answer\"> " << message << "</p>" << endl;
     cout << "<p class=\"prev__numbers\" id=\"prev\">Введённые ранее числа: " << prevAttempts << "</p>" << endl;
-    message = (currentAttempts > 0) ? ("У вас осталось " + to_string(currentAttempts) + " попыток.") : "Попыток больше нет.";
+    message = textAttempts(currentAttempts);
     cout << "<div style=\"text-align: center;\"><p style=\"font-weight: bold;\">" << message << "</p></div>" << endl;;
     cout << "</div>" << endl;
     cout << "</div>" << endl;
@@ -244,10 +270,25 @@ int main() {
     cout << "const submit = document.getElementById('submit');" << endl;
     cout << "const reset = document.getElementById('reset');" << endl;
     cout << "number.focus();" << endl;
-    if (currentAttempts == 0 || victory) {
+    if (currentAttempts == 0 || victory || cheat) 
+    {
         cout << "number.disabled = true;" << endl;
         cout << "submit.disabled = true;" << endl;
         cout << "reset.focus();" << endl;
+    }
+
+    if ((currentAttempts == 0 || cheat == true) && !victory)
+    {
+        cout << "number.style.backgroundColor = '#fb6262'" << endl;
+        cout << "document.querySelector('header').style.color = 'red';" << endl;
+        cout << "document.querySelector('header').style.textShadow = '0 0 5px #75b9f0, 0 0 10px #1100ff, 0 0 15px #4400ff, 0 0 20px #506fa0, 0 0 35px #2684b7, 0 0 40px #7a054b, 0 0 50px #376fe7, 0 0 75px #4a5084';" << endl;
+    }
+    else if(victory)
+    {
+        cout << "number.style.backgroundColor = '#f3de2c'" << endl;
+        cout << "document.querySelector('header').style.color = 'green';" << endl;
+        cout << "document.querySelector('header').style.textShadow = '0 0 5px #75b9f0, 0 0 10px #1100ff, 0 0 15px #4400ff, 0 0 20px #506fa0, 0 0 35px #2684b7, 0 0 40px #7a054b, 0 0 50px #376fe7, 0 0 75px #4a5084';" << endl;
+
     }
     cout << "});" << endl;
     cout << "</script>" << endl;
